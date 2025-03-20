8757a8f865ca7170792dedbcff739e3d76aa0c6d
ed3642dev <ed3642dev@gmail.com>
2025-03-20 16:15:38 -0400

    fix

--- app/(app-router)/api/ably-token/route.ts
+++ app/(app-router)/api/ably-token/route.ts
@@ -1,15 +1,11 @@
-import { auth } from '@/lib/authentication/auth'
 import Ably from 'ably'
 
 export async function GET() {
   const client = new Ably.Rest(process.env.ABLY_API_KEY!)
 
-  const session = await auth()
-  const userId = session?.user?.id
-
   // Create a token request for the client
   const tokenRequestData = await client.auth.createTokenRequest({
-    clientId: `purple-win-client-${userId}`,
+    clientId: `purple-win-client`,
   })
 
   // Return the token request data to the client
