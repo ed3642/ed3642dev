56cef64aef4e282777301f1ab05b2107e8e4876f
ed3642dev <ed3642dev@gmail.com>
2025-05-05 18:31:30 -0400

    img update

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,9 +4,11 @@ import Image from 'next/image'
 const Logo: React.FC = () => {
   return (
     <Link href="/">
-      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+      <div className="hover:opacity-75 transition items-center gap-x-2 flex">
         <Image src="/media/favicon.svg" alt="Logo" height={30} width={30} />
-        <p className="text-lg text-gray-200 pb-1">ed3642dev</p>
+        <p className="text-xl font-bold text-gray-200 font-mono tracking-tight items-center">
+          ed3642dev
+        </p>
       </div>
     </Link>
   )
